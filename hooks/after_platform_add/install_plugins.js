b42bb244b739ee9167df0afd519354530785dc2a
Andrea Gherardi <a.gherardi85@gmail.com>
2014-08-03 23:41:14 +1000

    Fixed build scripts and plugins (hopefully)

--- hooks/after_platform_add/install_plugins.js
+++ hooks/after_platform_add/install_plugins.js
@@ -1,10 +1,19 @@
 #!/usr/bin/env node
 
-//this hook installs all your plugins
+/**
+* A hook to automatically install a list cordova plugins.
+* Plugins are listed below in the `pluginlist` array.
+* A forEach loop iterates on the list and runs the `cordova plugin add` command, passing the name of the plugin as a parameter.
+*/
+
+// Provides nice colors in the console
+var colors = require('colors');
+// The folder we are working in
+var rootdir = process.argv[2];
 
 // add your plugins to this list--either the identifier, the filesystem location or the URL
 var pluginlist = [
-  // "com.ionic.keyboard",
+  "com.ionic.keyboard",
   "org.apache.cordova.console",
   "org.apache.cordova.device",
   "org.apache.cordova.file",
@@ -12,17 +21,21 @@ var pluginlist = [
   "org.apache.cordova.statusbar"
 ];
 
-// no need to configure below
+// No need to configure below
 
 var fs = require('fs');
 var path = require('path');
-var sys = require('sys')
+var sys = require('sys');
 var exec = require('child_process').exec;
 
 function puts(error, stdout, stderr) {
-    sys.puts(stdout)
+    sys.puts(stdout);
 }
 
+console.log(("\n- AFTER-PLATFORM-ADD HOOK - ("+ rootdir +"/install_plugins.js) - Installing plugins for each platform         \n").inverse);
+
 pluginlist.forEach(function(plug) {
-    exec("cordova plugin add " + plug, puts);
+  exec("cordova plugin add " + plug, puts);
 });
+
+console.log("\n                                                                                                              \n".inverse);
--- hooks/after_prepare/resource_files.js
+++ hooks/after_prepare/resource_files.js
@@ -1,63 +1,63 @@
 #!/usr/bin/env node
-//
-// This hook copies various resource files from our version control system directories into the appropriate platform specific location
-//
-// configure all the files to copy.  Key of object is the source file, value is the destination location.  It's fine to put all platforms' icons and splash screen files here, even if we don't build for all platforms on each developer's box.
-var filestocopy = [{
-    "config/android/drawable/icon.png": "platforms/android/res/drawable/icon.png"
-}, {
-    "config/android/drawable-hdpi/icon.png": "platforms/android/res/drawable-hdpi/icon.png"
-}, {
-    "config/android/drawable-ldpi/icon.png": "platforms/android/res/drawable-ldpi/icon.png"
-}, {
-    "config/android/drawable-mdpi/icon.png": "platforms/android/res/drawable-mdpi/icon.png"
-}, {
-    "config/android/drawable-xhdpi/icon.png": "platforms/android/res/drawable-xhdpi/icon.png"
-}, {
-    "config/android/drawable/splash.png": "platforms/android/res/drawable/splash.png"
-}, {
-    "config/android/drawable-hdpi/splash.png": "platforms/android/res/drawable-hdpi/splash.png"
-}, {
-    "config/android/drawable-ldpi/splash.png": "platforms/android/res/drawable-ldpi/splash.png"
-}, {
-    "config/android/drawable-mdpi/splash.png": "platforms/android/res/drawable-mdpi/splash.png"
-}, {
-    "config/android/drawable-xhdpi/splash.png": "platforms/android/res/drawable-xhdpi/splash.png"
-}, {
-    "config/ios/Resources/icons/icon-72.png": "platforms/ios/RecyclePedia/Resources/icons/icon-72.png"
-}, {
-    "config/ios/Resources/icons/icon.png": "platforms/ios/RecyclePedia/Resources/icons/icon.png"
-}, {
-    "config/ios/Resources/icons/icon@2x.png": "platforms/ios/RecyclePedia/Resources/icons/icon@2x.png"
-}, {
-    "config/ios/Resources/icons/icon-72@2x.png": "platforms/ios/RecyclePedia/Resources/icons/icon-72@2x.png"
-}, {
-    "config/ios/Resources/splash/Default@2x~iphone.png": "platforms/ios/RecyclePedia/Resources/splash/Default@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default-568h@2x~iphone.png": "platforms/ios/RecyclePedia/Resources/splash/Default-568h@2x~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default~iphone.png": "platforms/ios/RecyclePedia/Resources/splash/Default~iphone.png"
-}, {
-    "config/ios/Resources/splash/Default-Portrait~ipad.png": "platforms/ios/RecyclePedia/Resources/splash/Default-Portrait~ipad.png"
-}, {
-    "config/ios/Resources/splash/Default-Portrait@2x~ipad.png": "platforms/ios/RecyclePedia/Resources/splash/Default-Portrait@2x~ipad.png"
-}, ];
+
+/**
+* This hook copies various resource files from our version control system directories into the appropriate platform specific location
+* configure all the files to copy.  Key of object is the source file, value is the destination location.
+* It's fine to put all platforms' icons and splash screen files here, even if we don't build for all platforms on each developer's box.
+*/
 
 var fs = require('fs');
 var path = require('path');
-
-// no need to configure below
+// Provides nice colors in the console
+var colors = require('colors');
+// The folder we are working in
 var rootdir = process.argv[2];
 
+// List of assets to copy: {source: destination}
+var filestocopy = [
+  // Android - icons
+  {"config/android/drawable/icon.png": "platforms/android/res/drawable/icon.png"},
+  {"config/android/drawable-hdpi/icon.png": "platforms/android/res/drawable-hdpi/icon.png"},
+  {"config/android/drawable-ldpi/icon.png": "platforms/android/res/drawable-ldpi/icon.png"},
+  {"config/android/drawable-mdpi/icon.png": "platforms/android/res/drawable-mdpi/icon.png"},
+  {"config/android/drawable-xhdpi/icon.png": "platforms/android/res/drawable-xhdpi/icon.png"},
+
+  // Android - splashscreens
+  {"config/android/drawable/splash.png": "platforms/android/res/drawable/splash.png"},
+  {"config/android/drawable-hdpi/splash.png": "platforms/android/res/drawable-hdpi/splash.png"},
+  {"config/android/drawable-ldpi/splash.png": "platforms/android/res/drawable-ldpi/splash.png"},
+  {"config/android/drawable-mdpi/splash.png": "platforms/android/res/drawable-mdpi/splash.png"},
+  {"config/android/drawable-xhdpi/splash.png": "platforms/android/res/drawable-xhdpi/splash.png"},
+
+  // iOS - icons
+  {"config/ios/Resources/icons/icon-72.png": "platforms/ios/RecyclePedia/Resources/icons/icon-72.png"},
+  {"config/ios/Resources/icons/icon.png": "platforms/ios/RecyclePedia/Resources/icons/icon.png"},
+  {"config/ios/Resources/icons/icon@2x.png": "platforms/ios/RecyclePedia/Resources/icons/icon@2x.png"},
+  {"config/ios/Resources/icons/icon-72@2x.png": "platforms/ios/RecyclePedia/Resources/icons/icon-72@2x.png"},
+
+  // iOS - splashscreens
+  {"config/ios/Resources/splash/Default@2x~iphone.png": "platforms/ios/RecyclePedia/Resources/splash/Default@2x~iphone.png"},
+  {"config/ios/Resources/splash/Default-568h@2x~iphone.png": "platforms/ios/RecyclePedia/Resources/splash/Default-568h@2x~iphone.png"},
+  {"config/ios/Resources/splash/Default~iphone.png": "platforms/ios/RecyclePedia/Resources/splash/Default~iphone.png"},
+  {"config/ios/Resources/splash/Default-Portrait~ipad.png": "platforms/ios/RecyclePedia/Resources/splash/Default-Portrait~ipad.png"},
+  {"config/ios/Resources/splash/Default-Portrait@2x~ipad.png": "platforms/ios/RecyclePedia/Resources/splash/Default-Portrait@2x~ipad.png"}
+];
+
+console.log("\n- AFTER-PREPARE HOOK >>> Copying icons and splash screens for each platform                                       \n".inverse);
+
 filestocopy.forEach(function(obj) {
-    Object.keys(obj).forEach(function(key) {
-        var val = obj[key];
-        var srcfile = path.join(rootdir, key);
-        var destfile = path.join(rootdir, val);
-        console.log("copying "+srcfile+" to "+destfile);
-        var destdir = path.dirname(destfile);
-        if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
-            fs.createReadStream(srcfile).pipe(fs.createWriteStream(destfile));
-        }
-    });
+  Object.keys(obj).forEach(function(key) {
+    var val = obj[key];
+    var srcfile = path.join(rootdir, key);
+    var destfile = path.join(rootdir, val);
+    var destdir = path.dirname(destfile);
+
+    console.log((">>> Copying "+ srcfile +" to "+ destfile).green);
+
+    if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
+      fs.createReadStream(srcfile).pipe(fs.createWriteStream(destfile));
+    }
+  });
 });
+
+console.log("\n                                                                                                                  \n".inverse);
