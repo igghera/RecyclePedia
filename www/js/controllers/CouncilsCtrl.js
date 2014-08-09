5d0bbd009d27f567c68349a3f5823606c46e812a
Andrea Gherardi <a.gherardi85@gmail.com>
2014-08-09 14:29:44 +1000

    Added tooltips for initial tutorial

--- www/js/controllers/CouncilsCtrl.js
+++ www/js/controllers/CouncilsCtrl.js
@@ -1,5 +1,13 @@
+/**
+* Controller for Councils view.
+* This view shows:
+*
+* - a list of councils the user can choose from
+* - a search field to filter the list of councils
+* - a footer with 4 "standard" bins configurations
+*/
 angular.module('recyclepedia.controllers')
-.controller('CouncilsCtrl', function($rootScope, $scope, ApiService, $location, $ionicLoading) {
+.controller('CouncilsCtrl', function($rootScope, $scope, ApiService, $location, $ionicLoading, $ionicPopover, $timeout) {
   // Our 4 standard configurations (quite hard-coded)
   $scope.standardConfigs = [];
   // Display loading indicator
@@ -32,26 +40,97 @@ angular.module('recyclepedia.controllers')
     $rootScope.$broadcast('council-changed', standardConfig.name);
     $location.path('/app/categories');
   };
-
   /**
-  * Load list of councils list from API
+  * Load list of councils from API
   */
   ApiService.getCouncils().then(function (response) {
     angular.forEach(response.data.response, function(council) {
       // If council is actually a configuration we hide if from list and put it in the footer
-
       if(council.name.indexOf('+') < 0) {
         council.logoUrl = 'http://recyclesmart.com.au' + council.logo.logo.url;
         $scope.councils.push(council);
       } else {
         $scope.standardConfigs.push(council);
       }
-
+      // Hide loading view
       $ionicLoading.hide();
     });
   });
-
+  // Fired when user taps the CANCEL button next to the search field
   $scope.clearSearchField = function() {
     $scope.search.council.name = '';
   };
-})
+
+  $scope.openStep1 = function() {
+    $timeout(function() {
+      $scope.tutorialStep1.show(document.querySelector('.custom-search-icon'));
+    }, 500);
+  };
+
+  $scope.closeStep1 = function() {
+    $scope.tutorialStep1.hide();
+    $scope.openStep2();
+  };
+
+  $scope.openStep2 = function() {
+    $scope.tutorialStep2.show(document.querySelector('.standard-configs'));
+  };
+
+  $scope.closeStep2 = function() {
+    $scope.tutorialStep2.hide();
+    $scope.openStep3();
+  };
+
+  $scope.openStep3 = function() {
+    $scope.tutorialStep3.show(document.querySelector('.ion-navicon'));
+  };
+
+  $scope.closeStep3 = function() {
+    $scope.tutorialStep3.hide();
+    window.localStorage.showTutorialCouncilView = false;
+  };
+
+  // Load tutorial popovers
+  $ionicPopover.fromTemplateUrl('tutorial-step-1.html', {
+    scope: $scope,
+    animation: 'fade-in',
+    backdropClickToClose: false,
+    popoverPosition: 'bottom'
+  }).then(function(popover) {
+    $scope.tutorialStep1 = popover;
+    $scope.startTutorial();
+  });
+
+  $ionicPopover.fromTemplateUrl('tutorial-step-2.html', {
+    scope: $scope,
+    animation: 'fade-in',
+    backdropClickToClose: false,
+    popoverPosition: 'top'
+  }).then(function(popover) {
+    $scope.tutorialStep2 = popover;
+  });
+
+  $ionicPopover.fromTemplateUrl('tutorial-step-3.html', {
+    scope: $scope,
+    animation: 'fade-in',
+    backdropClickToClose: false,
+    popoverPosition: 'bottom'
+  }).then(function(popover) {
+    $scope.tutorialStep3 = popover;
+  });
+
+  $scope.startTutorial = function() {
+    var showTutorialCouncilView = angular.fromJson(window.localStorage.showTutorialCouncilView);
+
+    if(angular.isUndefined(showTutorialCouncilView) || showTutorialCouncilView === true) {
+      $scope.openStep1();
+    }
+  };
+
+  //Cleanup the modal when we're done with it
+  $scope.$on('$destroy', function() {
+    $scope.tutorialStep1.remove();
+    $scope.tutorialStep2.remove();
+    $scope.tutorialStep3.remove();
+  });
+});
